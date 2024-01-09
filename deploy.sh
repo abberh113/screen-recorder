b51e82dde4bbebcdbf2c08013cf3ad3737573de3
unknown <abberh113@gmail.com>
2024-01-09 22:09:06 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env sh
-
+# abort on errors
 set -e
-npm run build
-
-cd dist
-
-git init
-git add  -A
-git commit -m 'New Deployment'
-git push -f git@https://github.com/abberh113/screen-recorder.git master:gh-pages
-cd -
+# build
